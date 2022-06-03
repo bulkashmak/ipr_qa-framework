ebe67519f0d84e3e015f5990806599e4b0310776
Bulat Maskurov <mbmhd2015@gmail.com>
2022-06-03 15:43:20 +0300

    накидал базовый тест

--- src/test/java/ru/bulkashmak/ui/TrelloTest.java
+++ src/test/java/ru/bulkashmak/ui/TrelloTest.java
@@ -2,16 +2,18 @@ package ru.bulkashmak.ui;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import ru.bulkashmak.BaseTestUI;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 @DisplayName("UI тесты Trello")
-public class TestTrello {
+public class TrelloTest extends BaseTestUI {
 
     @Test
     public void loginTest() {
-        open("https://trello.com/");
+
+        loginPage.loginToOK();
 
     }
 }
