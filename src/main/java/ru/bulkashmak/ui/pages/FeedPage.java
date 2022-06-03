d8088b6806ecc22ce5231af8d3f6cb64ff429891
Bulat Maskurov <mbmhd2015@gmail.com>
2022-06-03 15:42:56 +0300

    FeedPage.java

---
+++ src/main/java/ru/bulkashmak/ui/pages/FeedPage.java
@@ -0,0 +1,10 @@
+package ru.bulkashmak.ui.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class FeedPage extends BasePage {
+
+
+}
