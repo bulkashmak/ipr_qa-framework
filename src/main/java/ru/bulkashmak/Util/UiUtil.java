998c9d59b6b172010b1a0be8fd4a9e20ac625ee4
Bulat Maskurov <mbmhd2015@gmail.com>
2022-06-12 13:24:30 +0300

    рефакторинг

--- src/main/java/ru/bulkashmak/Util/UiUtil.java
+++ src/main/java/ru/bulkashmak/Util/UiUtil.java
@@ -9,11 +9,9 @@ public class UiUtil {
         int rightLimit = 122; // letter 'z'
         Random random = new Random();
 
-        String generatedString = random.ints(leftLimit, rightLimit + 1)
+        return random.ints(leftLimit, rightLimit + 1)
                 .limit(targetStringLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
-
-        return generatedString;
     }
 }
