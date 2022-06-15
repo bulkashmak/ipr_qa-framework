c4ee55667d09fd008469cd07c86e04d557972ed9
Bulat Maskurov <mbmhd2015@gmail.com>
2022-06-15 14:06:03 +0300

    база для api тестов

---
+++ src/main/java/ru/bulkashmak/ui/properties/ApiProperties.java
@@ -0,0 +1,21 @@
+package ru.bulkashmak.ui.properties;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ApiProperties {
+    private static Properties properties = new Properties();
+
+    static {
+        try(InputStream inputStream = UiProperties.class.getClassLoader().getResourceAsStream("api.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String nameProperty) {
+        return properties.getProperty(nameProperty);
+    }
+}
---
+++ src/test/java/ru/bulkashmak/api/PflbApiTest.java
@@ -0,0 +1,14 @@
+package ru.bulkashmak.api;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PflbApiTest {
+
+    @Test
+    @DisplayName("Получение всех пользователей")
+    public void getAllUsersTest() {
+
+
+    }
+}
