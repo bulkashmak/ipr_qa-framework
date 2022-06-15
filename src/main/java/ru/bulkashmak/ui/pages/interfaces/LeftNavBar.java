6993428dd5e4fb092e32dec16c7b6e2739d52123
Bulat Maskurov <mbmhd2015@gmail.com>
2022-06-15 12:57:42 +0300

    2. Зачем выделять enum для элементов?

--- src/main/java/ru/bulkashmak/ui/pages/interfaces/LeftNavBar.java
+++ src/main/java/ru/bulkashmak/ui/pages/interfaces/LeftNavBar.java
@@ -1,7 +1,6 @@
 package ru.bulkashmak.ui.pages.interfaces;
 
 import com.codeborne.selenide.Condition;
-import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.bulkashmak.ui.pages.ProfilePage;
@@ -16,24 +15,12 @@ public interface LeftNavBar {
     default ProfilePage goToProfilePage() {
         LOGGER.info("Переход на страницу 'Профиль'");
 
-        $x(LeftNavBarElements.PROFILE.getXpath()).click();
+        $x("//*[@id='hook_Block_SideNavigation']//*[@data-l='t,userPage']").click();
 
         assertTrue($x("//*[@id='hook_Block_UserProfileCoverWrapper']")
                 .shouldBe(Condition.visible).isDisplayed(),
                 "Не произошел переход на страницу Профиля");
 
         return new ProfilePage();
-    }
-
-    @Getter
-    enum LeftNavBarElements {
-
-        PROFILE("//*[@id='hook_Block_SideNavigation']//*[@data-l='t,userPage']");
-
-        private final String xpath;
-
-        LeftNavBarElements(String xpath) {
-            this.xpath = xpath;
-        }
     }
 }
