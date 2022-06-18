646dd83a84a0cf776613e21d81ff75c30bc584eb
Bulat Maskurov <mbmhd2015@gmail.com>
2022-06-18 15:51:31 +0300

    шаги получения пользователя по id и создание пользователя

--- src/test/java/ru/bulkashmak/api/PflbApiTest.java
+++ src/test/java/ru/bulkashmak/api/PflbApiTest.java
@@ -4,20 +4,35 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.bulkashmak.api.config.BaseTest;
+import ru.bulkashmak.api.models.user.UserRequest;
 import ru.bulkashmak.api.models.user.UserResponse;
 import ru.bulkashmak.api.steps.PflbApiStep;
+import ru.bulkashmak.api.testdata.TestDataGenerator;
 
 import java.util.List;
 
 public class PflbApiTest extends BaseTest {
 
+    private static final TestDataGenerator testDataGenerator = new TestDataGenerator();
+
     @Test
     @DisplayName("Получение всех пользователей")
     public void getAllUsersTest() {
         PflbApiStep step = new PflbApiStep();
+        List<UserResponse> usersRs = step.getUsers();
 
-        List<UserResponse> users = step.getUsers();
+        usersRs.forEach(x -> assertNotNull(x.getId(),
+                String.format("user '%s' id is null", x.getFirstName())));
+    }
 
-        users.forEach(x -> assertNotNull(x.getId(), String.format("user '%s' id is null", x.getFirstName())));
+    @Test
+    @DisplayName("Создание пользователя")
+    public void postAddUserTest() {
+        PflbApiStep step = new PflbApiStep();
+        UserRequest userRq = testDataGenerator.generateUser();
+        UserResponse userRs = step.postAddUser(userRq);
+
+        assertEquals(userRs, step.getUserById(userRs.getId()),
+                "пользователь создан некорректно");
     }
 }
