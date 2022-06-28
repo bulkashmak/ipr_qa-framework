250bccf34fc5b3d3627277d1b2b21616c5bb1848
Bulat Maskurov <mbmhd2015@gmail.com>
2022-06-28 17:51:57 +0300

    рефакторинг шагов pflbapi

--- src/main/java/ru/bulkashmak/api/steps/PflbApiStep.java
+++ src/main/java/ru/bulkashmak/api/steps/PflbApiStep.java
@@ -5,35 +5,53 @@ import org.slf4j.LoggerFactory;
 import ru.bulkashmak.api.models.user.UserRequest;
 import ru.bulkashmak.api.models.user.UserResponse;
 import ru.bulkashmak.api.requests.PflbApiRequest;
+import ru.bulkashmak.api.testdata.TestDataGenerator;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PflbApiStep {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PflbApiStep.class);
+    private static final TestDataGenerator testDataGenerator = new TestDataGenerator();
 
-    public List<UserResponse> getUsers() {
+    public List<UserResponse> getUsersStep() {
         PflbApiRequest request = new PflbApiRequest();
+        List<UserResponse> usersResponse = request.getUsers();
 
-        return request.getUsers();
+        usersResponse.forEach(x -> assertNotNull(x.getId(),
+                String.format("ID пользователя '%s' равен null", x.getFirstName())));
+
+        return usersResponse;
     }
 
-    public UserResponse getUserById(Integer userId) {
+    public UserResponse postAddUserStep() {
         PflbApiRequest request = new PflbApiRequest();
+        UserRequest userRequest = testDataGenerator.generateUser();
+        UserResponse userResponse = request.postAddUser(userRequest);
 
-        return request.getUserById(userId);
+        assertEquals(userResponse, request.getUserById(userResponse.getId()),
+                "пользователь создан некорректно");
+
+        return userResponse;
     }
 
-    public UserResponse postAddUser(UserRequest userRequest) {
+    public UserResponse postUserByIdMoneyStep() {
         PflbApiRequest request = new PflbApiRequest();
+        List<UserResponse> usersResponse = request.getUsers();
+        UserResponse randomUser = usersResponse.get(new Random().nextInt(usersResponse.size()));
+        BigDecimal money = TestDataGenerator.generateRandomMoney();
+        UserResponse response = request.postUserByIdMoney(randomUser.getId(), money);
 
-        return request.postAddUser(userRequest);
-    }
+        BigDecimal expectedResult = randomUser.getMoney().add(money);
 
-    public UserResponse postUserByIdMoney(Integer userId, BigDecimal userMoney) {
-        PflbApiRequest request = new PflbApiRequest();
+        assertEquals(0, expectedResult.compareTo(response.getMoney()),
+                "Сумма денег пользователя изменена некорректно");
 
-        return request.postUserByIdMoney(userId, userMoney);
+        return response;
     }
 }
--- src/test/java/ru/bulkashmak/api/PflbApiTest.java
+++ src/test/java/ru/bulkashmak/api/PflbApiTest.java
@@ -3,17 +3,8 @@ package ru.bulkashmak.api;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.bulkashmak.api.config.BaseTest;
-import ru.bulkashmak.api.models.user.UserRequest;
-import ru.bulkashmak.api.models.user.UserResponse;
 import ru.bulkashmak.api.steps.PflbApiStep;
 import ru.bulkashmak.api.testdata.TestDataGenerator;
-
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PflbApiTest extends BaseTest {
 
@@ -23,35 +14,20 @@ public class PflbApiTest extends BaseTest {
     @DisplayName("Получение всех пользователей")
     public void getAllUsersTest() {
         PflbApiStep step = new PflbApiStep();
-        List<UserResponse> usersRs = step.getUsers();
-
-        usersRs.forEach(x -> assertNotNull(x.getId(),
-                String.format("user's '%s' id is null", x.getFirstName())));
+        step.getUsersStep();
     }
 
     @Test
     @DisplayName("Создание пользователя")
     public void postAddUserTest() {
         PflbApiStep step = new PflbApiStep();
-        UserRequest userRq = testDataGenerator.generateUser();
-        UserResponse userRs = step.postAddUser(userRq);
-
-        assertEquals(userRs, step.getUserById(userRs.getId()),
-                "пользователь создан некорректно");
+        step.postAddUserStep();
     }
 
     @Test
     @DisplayName("Изменение суммы у пользователя")
     public void postUserMoneyTest() {
         PflbApiStep step = new PflbApiStep();
-        List<UserResponse> usersRs = step.getUsers();
-        UserResponse randomUser = usersRs.get(new Random().nextInt(usersRs.size()));
-        BigDecimal money = TestDataGenerator.generateRandomMoney();
-        UserResponse response = step.postUserByIdMoney(randomUser.getId(), money);
-
-        BigDecimal expectedResult = randomUser.getMoney().add(money);
-
-        assertEquals(0, expectedResult.compareTo(response.getMoney()),
-                "Сумма денег пользователя изменена некорректно");
+        step.postUserByIdMoneyStep();
     }
 }
